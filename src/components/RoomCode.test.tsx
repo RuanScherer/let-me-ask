e107f262b4d46011776a58c163608af200d4f9d0
Ruan Scherer <ruan.scherer@outlook.com>
2021-06-26 20:37:59 -0300

    :tada: tests: add Room Code component UI tests

---
+++ src/components/RoomCode.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { RoomCode } from './RoomCode';
+
+describe('Room Code Component', () => {
+	test('Show room code', () => {
+		const code = '-Ab-1jH-';
+		render(<RoomCode roomCode={code} />);
+
+		expect(screen.getByText(/-Ab-1jH-/)).toBeInTheDocument();
+	});
+
+	test('Snapshot must always be the same', () => {
+		const { container } = render(<RoomCode roomCode="-Ab-1jH-" />);
+
+		expect(container.firstChild).toMatchSnapshot();
+	});
+});
+
+export { };
